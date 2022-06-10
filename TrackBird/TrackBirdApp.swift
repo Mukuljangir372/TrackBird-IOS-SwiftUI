e6488aeecfdbb6cf9ecf70ff0885d533d9109ca9
mukul372 <mukul@scogo.in>
2022-06-10 10:04:08 +0530

    Initial Commit

---
+++ TrackBird/TrackBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackBirdApp.swift
+//  TrackBird
+//
+//  Created by Mukul Jangir on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
